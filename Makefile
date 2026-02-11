c13d728eb5bb3b901a80fd30de2deb311b68d781
Leonardo Uieda <leo@uieda.com>
2026-02-11 12:30:38 -0300

    Adiciona scripts para calcular variação temperatura e plotar
    Adiciona também as regras no Makefile para gerar a principal figura
    do artigo.

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 #alvo: dependencia1 dependencia2
 #	comando que gera o alvo a partir das dependencias
 
-.PHONY: clean
+.PHONY: clean all
+
+all: figuras/taxas_variacao.png
 
 data/temperature-data.zip: code/baixa_dados.py
 	python code/baixa_dados.py
@@ -10,5 +12,13 @@ results/npaises.txt: data/temperature-data.zip code/conta_paises.sh
 	mkdir -p results
 	bash code/conta_paises.sh > results/npaises.txt
 
+results/variacao_temperatura.csv: data/temperature-data.zip code/variacao_temperatura.py
+	mkdir -p results
+	python code/variacao_temperatura.py > results/variacao_temperatura.csv
+
+figuras/taxas_variacao.png: code/plota_dados.py results/variacao_temperatura.csv
+	mkdir -p figuras
+	python code/plota_dados.py
+
 clean:
-	rm -r data results
+	rm -r data results figuras
---
+++ code/variacao_temperatura.py
@@ -0,0 +1,31 @@
+"""
+Calcula a taxa de variação de temperatura dos últimos 5 anos para todos os
+países.
+
+Os resultados são impressos no STDOUT.
+"""
+
+import pathlib
+import pandas as pd
+import numpy as np
+
+
+data_folder = pathlib.Path("data/temperature")
+
+# Acha todos os arquivos .csv da pasta
+data_files = data_folder.glob("*.csv")
+
+# Começa a saída com um cabeçalho
+print("variacao_C_por_ano,pais")
+
+for file_path in data_files:
+    # Lê o csv ignorando comentários
+    data = pd.read_csv(file_path, comment="#")
+    # Filtra dados dos últimos cinco anos
+    last_five_years = data[data.year_decimal > data.year_decimal.iloc[-1] - 5]
+    # Regressão linear
+    coefficients = np.polyfit(
+        last_five_years.year_decimal, last_five_years.temperature_C, 1
+    )
+    # Impressão de resultados
+    print(f'{coefficients[0]:.3f},"{file_path.stem.replace("-", " ").title()}"')
